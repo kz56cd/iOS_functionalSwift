4c32b87c880a99f751a0944ba5c3d5a03a76c70e
kz56cd <kz56cd@gmail.com>
2017-11-26 23:27:29 +0900

    Update Optional(02) playground

--- functionalSwiftPlayground.playground/Pages/オプショナル型 (02).xcplaygroundpage/Contents.swift
+++ functionalSwiftPlayground.playground/Pages/オプショナル型 (02).xcplaygroundpage/Contents.swift
@@ -5,5 +5,36 @@
 
 import Foundation
 
+struct Order {
+    let orderNumber: Int
+    let person: Person?
+}
+
+struct Person {
+    let name: String
+    let address: Address?
+}
+
+struct Address {
+    let streetName: String
+    let city: String
+    let state: String?
+}
+
+let order = Order(
+    orderNumber: 42,
+    person: nil
+)
+
+// NOTE: ネストしたデータを取り出したい場合、オプショナルチェーンで取り出すのが楽
+if let myState = order.person?.address?.state {
+    print("\(myState)")
+} else {
+    print("unknown state.")
+}
+
+// WIP
+
+
 // stub
 //: [Next](@next)
